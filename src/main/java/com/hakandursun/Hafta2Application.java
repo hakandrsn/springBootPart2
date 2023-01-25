af331b315c01492b121a5884113a5596f3dec65e
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2023-01-25 19:35:28 +0300

    application properties

---
+++ src/main/java/com/hakandursun/Hafta2Application.java
@@ -0,0 +1,13 @@
+package com.hakandursun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hafta2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hafta2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/hakandursun/utils/PermaLink.java
@@ -0,0 +1,2 @@
+package com.hakandursun.utils;public class PermaLink {
+}
