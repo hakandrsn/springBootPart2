e22a6aa4c008b4f7f36995752a89b5adf57b32e6
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2023-01-25 20:09:03 +0300

    bazı detayla

---
+++ src/main/java/com/hakandursun/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.hakandursun.utils;
+
+public class ConfigProgramming {
+    //DevTools ==> liveReload
+    //file ==> settings ==> compiler ==> build project automatically
+    //file ==> Advanced settings > allow auto-make to start even if the project is not saved
+}
---
+++ src/main/java/com/hakandursun/utils/intellijShortCut.java
@@ -0,0 +1,9 @@
+package com.hakandursun.utils;
+
+public class intellijShortCut {
+/*    Setting ==> ctrl + alt + s
+    Run ==> ctrl + shift + f10
+    Alt + Enter ==> Quick Fix
+    Alt + Insert ==> Generate*/
+
+}
