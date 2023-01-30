990077802a2e32483b583462ee6f827abc2fa361
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2023-01-30 13:56:28 +0300

    modelden veri aktarıldı css ve js importları yapıldı thymeleaf kullanılarak

---
+++ src/main/java/com/hakandursun/controller/ThymeleadController.java
@@ -0,0 +1,32 @@
+package com.hakandursun.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ThymeleadController {
+
+
+    //http://localhost:8080/thymeleaf1
+    @GetMapping("/thymeleaf1")
+  //  @ResponseBody
+    public String getThymeleaf1(){
+        return "thymeleaf1";
+    }
+
+    //http://localhost:8080/thymeleaf2
+    @GetMapping("/xmlnameserver")
+    public String getThymeleaf2(){
+        return "xmlnameserver";
+    }
+
+    //http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model(Model model){
+        model.addAttribute("name","Hakan");
+        return "thymeleaf";
+    }
+}
